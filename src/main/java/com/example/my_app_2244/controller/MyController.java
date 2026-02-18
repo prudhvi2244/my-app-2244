ec01962f86626d3dd0463d7c0c7a354841bfc665
Advith <raj.rajeev2244@gmail.com>
2026-02-19 00:49:10 +0530

    First Commit

--- src/main/java/com/example/my_app_2244/controller/MyController.java
+++ src/main/java/com/example/my_app_2244/controller/MyController.java
@@ -1,6 +1,7 @@
 package com.example.my_app_2244.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +11,10 @@ public class MyController {
     public String getMethod(){
         return "Hello, From Spring Boot API";
     }
+
+    @PostMapping
+    public String postMapping(){
+        return "Post Mapping";
+    }
+
 }
